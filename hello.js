42f5e20c9a84193258791c5593f125b84f32e983
surajgit036 <Surajkrch36@gmail.com>
2023-01-31 13:43:25 +0530

    first

---
+++ hello.js
@@ -0,0 +1,8 @@
+const mission = process.argv[2];    // index 2 means in terminal whtever u  pass in 2nd index it will get printed 
+
+if(mission ==='learn'){
+   console.log('Time to write some code');
+
+}else{
+    console.log(`Is ${mission} really was fun?`);
+}
